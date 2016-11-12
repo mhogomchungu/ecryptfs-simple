c457df105092efd3835a91c8a97d552f5d525f11
Francis Banyikwa <mhogomchungu@gmail.com>
2016-11-12 21:17:07 +0300

    try to fix a build issue in fedora 20

--- src/ecryptfs-simple.c
+++ src/ecryptfs-simple.c
@@ -71,6 +71,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <sys/types.h>
 #include <unistd.h>
 #include <limits.h>
+#include <linux/limits.h>
 #include <stdlib.h>
 #include <alloca.h>
 #include <sys/mount.h>
